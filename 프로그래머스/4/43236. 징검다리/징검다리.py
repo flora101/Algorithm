3f03fd97ea547d7c09bb2f423aef869421857608
Bae Geunyoung <94815293+flora101@users.noreply.github.com>
2024-08-15 16:37:17 +0900

    Update 징검다리.py

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,21 +1,21 @@
 # def solution(distance, rocks, n):
 #     answer = []
-#     left,right = 0, distance
-#     total = len(rocks)-n
+#     left,right = 1, distance
+#     rocks.append(distance)
 #     rocks.sort()
-    
-#     def check(middle):
-#         cnt = 0
-#         first = 0
-#         for i in rocks:
-#             if i-first>=middle:
-#                 cnt+=1
-#                 first = i
-#         return cnt
     
 #     while left<=right:
 #         mid = (left+right)//2
-#         if check(mid)<total:
+#         cnt = 0
+#         first = 0
+#         for i in rocks:
+#             if i-first<mid:
+#                 cnt+=1
+#             else:
+#                 first = i
+#             if cnt>n:
+#                 break
+#         if cnt>n:
 #             right = mid-1
 #         else:
 #             answer.append(mid)
@@ -24,30 +24,26 @@
 
 
 def solution(distance, rocks, n):
-    answer = 0
-    
+    answer = []
+    left,right = 0, distance
+    total = len(rocks)-n+1
     rocks.append(distance)
     rocks.sort()
     
-    start = 1 
-    end = distance 
+    def check(middle):
+        cnt = 0
+        first = 0
+        for i in rocks:
+            if i-first>=middle:
+                cnt+=1
+                first = i
+        return cnt
     
-    while start <= end:
-        mid = (start + end) // 2 
-        
-        previous = 0 
-        count = 0
-        for rock in rocks:
-            if rock - previous < mid:
-                count += 1
-                if count > n: break 
-            else:
-                previous = rock
-        
-        if count > n:
-            end = mid - 1 
+    while left<=right:
+        mid = (left+right)//2
+        if check(mid)<total:
+            right = mid-1
         else:
-            answer = mid 
-            start = mid + 1
-    
-    return answer
+            answer.append(mid)
+            left=mid+1
+    return max(answer)

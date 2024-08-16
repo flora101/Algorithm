e46406c24c346cdcc8aee92bc90f9f94746f0581
Bae Geunyoung <94815293+flora101@users.noreply.github.com>
2024-08-16 13:38:18 +0900

    [Silver I] Title: 귀여운 라이언, Time: 512 ms, Memory: 51972 KB -BaekjoonHub

---
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
@@ -0,0 +1,21 @@
+    
+n,k = map(int,input().split())
+doll = list(map(int,input().split()))
+left = 0
+cnt = 0 #1의 개수
+answer = 2147000000 #가장 작은 집합 크기
+for right in range(n):
+    if doll[right]==1:
+        cnt+=1
+    while cnt>k:
+        if doll[left]==1:
+            cnt-=1
+        left+=1
+    if cnt == k:
+        while doll[left]==2:
+            left+=1
+        answer = min(answer,right-left+1)
+if answer == 2147000000:
+    print(-1)
+else:
+    print(answer)

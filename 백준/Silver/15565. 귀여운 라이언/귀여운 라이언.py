68857a9e7a94d2c18076b4b26fdcd932caf972ed
Bae Geunyoung <94815293+flora101@users.noreply.github.com>
2024-08-16 13:45:01 +0900

    Update 귀여운 라이언.py

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.py
@@ -1,4 +1,24 @@
-    
+# n,k = map(int,input().split())
+# doll = list(map(int,input().split()))
+# left,right = 0,0
+# cnt = 0 #1의 개수
+# answer = 2147000000 #가장 작은 집합 크기
+# while right<n:
+#     if doll[right]==1:
+#         cnt+=1
+#     while cnt >= k:
+#         if cnt == k:
+#             answer = min(answer, right - left + 1)
+#         if doll[left] == 1:
+#             cnt -= 1
+#         left += 1
+#     right += 1
+# if answer ==2147000000:
+#     print("-1")
+# else:
+#     print(answer)
+
+
 n,k = map(int,input().split())
 doll = list(map(int,input().split()))
 left = 0
